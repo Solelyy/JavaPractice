4ab933b9ce742b79a1f2cc5c376f0e572e18240b
Jessa Gozun <jessanazarenedietagozun@gmail.com>
2025-09-02 00:03:03 +0800

    Update TypeCasting.java with new changes.

--- Java/src/extras/TypeCasting.java
+++ Java/src/extras/TypeCasting.java
@@ -4,7 +4,7 @@ public class TypeCasting {
     public static void main (String [] args){
         //Type Casting - data type to another data type
         //Widening Type Casting= Smaller to Bigger data type
-        //byte - short - int - long - float (15 decimals) - double (6-7 decimals)
+        //byte - short - int - long - float (approx. 7 decimals) - double (approx. 15 decimals)
         System.out.println("Type Casting");
         System.out.println("1. Widening Type Casting/ Implicit:");
         byte by= 45; //byte is up to 127
