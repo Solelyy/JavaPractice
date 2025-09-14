8f415375227681afac05f668d647b8027e06c1f6
Jessa Gozun <jessanazarenedietagozun@gmail.com>
2025-09-14 19:15:10 +0800

    Added HashMapDemo.java.

---
+++ Java/src/collections/map/HashMapDemo.java
@@ -0,0 +1,59 @@
+package collections.map;
+
+import java.util.Scanner;
+import java.util.HashMap;
+
+public class HashMapDemo {
+    /*
+    1. Ask the user to input a sentence.
+    2. Split the sentence into words.
+    3. Use a HashMap<String, Integer>:
+    Key → word
+    Value → frequency (how many times it appeared)
+    4. Print all words with their counts.
+    5. Let the user search for a specific word and display
+    its frequency (or show “not found”).
+     */
+    public static void main (String [] args) {
+        Scanner sc = new Scanner(System.in);
+
+        HashMap <String, Integer> wordCount = new HashMap<>();
+
+        //1.
+        System.out.print("Input a sentence: ");
+        String sentence = sc.nextLine();
+
+        //2.
+        String [] words  = sentence.split("\\W+");
+
+        //3.
+        System.out.println("Word and its count: ");
+        for (String word : words){
+            word = word.toLowerCase();
+            if (wordCount.containsKey(word)){
+                wordCount.put(word, wordCount.get(word) +1);
+            } else {
+                wordCount.put(word, 1);
+            }
+        }
+
+        //4.
+        for (var entry: wordCount.entrySet()) {
+            System.out.println(entry.getKey() + " -> " + entry.getValue());
+        }
+
+        /* other way:
+        wordCount.forEach((word, count) ->
+                System.out.println(word + "->" + count));*/
+
+        //5.
+        System.out.print("Search a word: ");
+        String searchWord= sc.next().toLowerCase();
+
+        if (wordCount.containsKey(searchWord)) {
+            System.out.println("Count: " + wordCount.get(searchWord));
+        } else {
+            System.out.println("Not found.");
+        }
+    }
+}
