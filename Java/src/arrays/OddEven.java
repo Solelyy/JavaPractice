962670feb550f53b6a062988a913ae183ed9bb28
Jessa Gozun <jessanazarenedietagozun@gmail.com>
2025-10-16 23:34:52 +0800

    Added an exercise.

---
+++ Java/src/arrays/OddEven.java
@@ -0,0 +1,25 @@
+package arrays;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class OddEven{
+    //Challenge: Identify if the integer given by the user is odd or even.
+    public static void main (String [] args){
+        Scanner sc = new Scanner(System.in);
+
+        try{
+            for (int i= 0; i < 5; i++) {
+                System.out.print("Enter a number: ");
+                int num= sc.nextInt();
+
+                if (num % 2 == 0) {
+                    System.out.println("Even");
+                } else {
+                    System.out.println("Odd");
+                }
+            }
+        } catch (InputMismatchException e){
+            System.out.println("Enter an integer only.");
+        }
+    }
+}
